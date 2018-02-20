41ff8bfb07256275e46eab4881bc8060005b4118
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2018-02-20 01:07:02 +0100

    added fasttext and word2vec embeddings,  refactored/cleaned pipelines

--- pipeline_config.py
+++ pipeline_config.py
@@ -49,11 +49,11 @@ SOLUTION_CONFIG = AttrDict({
                               'ngram_range': (1, 1),
                               'max_features': params.max_features_word
                               },
-    'glove_embeddings': {'pretrained_filepath': params.embedding_filepath,
-                         'max_features': params.max_features_word,
-                         'embedding_size': params.word_embedding_size
-                         },
-    'glove_dpcnn_network': {
+    'embeddings': {'pretrained_filepath': params.embedding_filepath,
+                   'max_features': params.max_features_word,
+                   'embedding_size': params.word_embedding_size
+                   },
+    'dpcnn_network': {
         'architecture_config': {'model_params': {'max_features': params.max_features_word,
                                                  'maxlen': params.maxlen_words,
                                                  'embedding_size': params.word_embedding_size,
@@ -82,8 +82,8 @@ SOLUTION_CONFIG = AttrDict({
                             },
         'callbacks_config': {'model_checkpoint': {
             'filepath': os.path.join(params.experiment_dir, 'checkpoints',
-                                     'glove_dpcnn_network',
-                                     'glove_dpcnn_network.h5'),
+                                     'dpcnn_network',
+                                     'best_model.h5'),
             'save_best_only': True,
             'save_weights_only': False},
             'lr_scheduler': {'gamma': params.gamma},
@@ -91,7 +91,7 @@ SOLUTION_CONFIG = AttrDict({
             'neptune_monitor': {},
         },
     },
-    'glove_scnn_network': {
+    'scnn_network': {
         'architecture_config': {'model_params': {'max_features': params.max_features_word,
                                                  'maxlen': params.maxlen_words,
                                                  'embedding_size': params.word_embedding_size,
@@ -119,8 +119,8 @@ SOLUTION_CONFIG = AttrDict({
                             },
         'callbacks_config': {'model_checkpoint': {
             'filepath': os.path.join(params.experiment_dir, 'checkpoints',
-                                     'glove_scnn_network',
-                                     'glove_scnn_network.h5'),
+                                     'scnn_network',
+                                     'best_model.h5'),
             'save_best_only': True,
             'save_weights_only': False},
             'lr_scheduler': {'gamma': params.gamma},
@@ -128,7 +128,7 @@ SOLUTION_CONFIG = AttrDict({
             'neptune_monitor': {},
         },
     },
-    'glove_lstm_network': {
+    'lstm_network': {
         'architecture_config': {'model_params': {'max_features': params.max_features_word,
                                                  'maxlen': params.maxlen_words,
                                                  'embedding_size': params.word_embedding_size,
@@ -155,8 +155,8 @@ SOLUTION_CONFIG = AttrDict({
                             },
         'callbacks_config': {'model_checkpoint': {
             'filepath': os.path.join(params.experiment_dir, 'checkpoints',
-                                     'glove_lstm_network',
-                                     'glove_lstm_network.h5'),
+                                     'lstm_network',
+                                     'best_model.h5'),
             'save_best_only': True,
             'save_weights_only': False},
             'lr_scheduler': {'gamma': params.gamma},
@@ -164,7 +164,7 @@ SOLUTION_CONFIG = AttrDict({
             'neptune_monitor': {},
         },
     },
-    'glove_gru_network': {
+    'gru_network': {
         'architecture_config': {'model_params': {'max_features': params.max_features_word,
                                                  'maxlen': params.maxlen_words,
                                                  'embedding_size': params.word_embedding_size,
@@ -191,79 +191,8 @@ SOLUTION_CONFIG = AttrDict({
                             },
         'callbacks_config': {'model_checkpoint': {
             'filepath': os.path.join(params.experiment_dir, 'checkpoints',
-                                     'glove_gru_network',
-                                     'glove_gru_network.h5'),
-            'save_best_only': True,
-            'save_weights_only': False},
-            'lr_scheduler': {'gamma': params.gamma},
-            'early_stopping': {'patience': params.patience},
-            'neptune_monitor': {},
-        },
-    },
-    'word_lstm_network': {
-        'architecture_config': {'model_params': {'max_features': params.max_features_word,
-                                                 'maxlen': params.maxlen_words,
-                                                 'embedding_size': params.word_embedding_size,
-                                                 'unit_nr': params.filter_nr,
-                                                 'repeat_block': params.repeat_block,
-                                                 'global_pooling': bool(params.global_pooling),
-                                                 'dense_size': params.dense_size,
-                                                 'repeat_dense': params.repeat_dense,
-                                                 'l2_reg_dense': params.l2_reg_dense,
-                                                 'use_prelu': bool(params.use_prelu),
-                                                 'use_batch_norm': bool(params.use_batch_norm),
-                                                 'dropout_lstm': params.dropout_lstm,
-                                                 'dropout_dense': params.dropout_dense
-                                                 },
-                                'optimizer_params': {'lr': params.lr,
-                                                     'momentum': params.momentum,
-                                                     'nesterov': True
-                                                     },
-                                },
-        'training_config': {'epochs': params.epochs_nr,
-                            'batch_size': params.batch_size_train,
-                            },
-        'callbacks_config': {'model_checkpoint': {
-            'filepath': os.path.join(params.experiment_dir, 'checkpoints', 'word_lstm_network',
-                                     'word_lstm_network.h5'),
-            'save_best_only': True,
-            'save_weights_only': False},
-            'lr_scheduler': {'gamma': params.gamma},
-            'early_stopping': {'patience': params.patience},
-            'neptune_monitor': {},
-        },
-    },
-    'word_dpcnn_network': {
-        'architecture_config': {'model_params': {'max_features': params.max_features_word,
-                                                 'maxlen': params.maxlen_words,
-                                                 'embedding_size': params.word_embedding_size,
-                                                 'trainable_embedding': bool(
-                                                     params.trainable_embedding),
-                                                 'filter_nr': params.filter_nr,
-                                                 'kernel_size': params.kernel_size,
-                                                 'repeat_block': params.repeat_block,
-                                                 'dense_size': params.dense_size,
-                                                 'repeat_dense': params.repeat_dense,
-                                                 'l2_reg_convo': params.l2_reg_convo,
-                                                 'l2_reg_dense': params.l2_reg_dense,
-                                                 'use_prelu': bool(params.use_prelu),
-                                                 'use_batch_norm': bool(params.use_batch_norm),
-                                                 'dropout_convo': params.dropout_convo,
-                                                 'dropout_dense': params.dropout_dense
-                                                 },
-                                'optimizer_params': {'lr': params.lr,
-                                                     'momentum': params.momentum,
-                                                     'nesterov': True
-                                                     },
-                                },
-        'training_config': {'epochs': params.epochs_nr,
-                            'shuffle': True,
-                            'batch_size': params.batch_size_train,
-                            },
-        'callbacks_config': {'model_checkpoint': {
-            'filepath': os.path.join(params.experiment_dir, 'checkpoints',
-                                     'word_dpcnn_network',
-                                     'word_dpcnn_network.h5'),
+                                     'gru_network',
+                                     'best_model.h5'),
             'save_best_only': True,
             'save_weights_only': False},
             'lr_scheduler': {'gamma': params.gamma},
@@ -298,7 +227,7 @@ SOLUTION_CONFIG = AttrDict({
         'callbacks_config': {'model_checkpoint': {
             'filepath': os.path.join(params.experiment_dir, 'checkpoints',
                                      'char_vdcnn_network',
-                                     'char_vdcnn_network.h5'),
+                                     'best_model.h5'),
             'save_best_only': True,
             'save_weights_only': False},
             'lr_scheduler': {'gamma': params.gamma},
@@ -313,9 +242,6 @@ SOLUTION_CONFIG = AttrDict({
                                        'max_iter': params.max_iter,
                                        'n_jobs': params.num_workers,
                                        },
-    'svc_multilabel': {'label_nr': 6,
-                       'C': params.svm_C,
-                       },
     'catboost_ensemble': {'label_nr': 6,
                           'iterations': params.catboost__iterations,
                           'learning_rate': params.catboost__learning_rate,
--- steps/keras/models.py
+++ steps/keras/models.py
@@ -3,6 +3,7 @@ import shutil
 import numpy as np
 from keras.models import load_model
 from sklearn.externals import joblib
+from gensim.models import KeyedVectors
 
 from steps.base import BaseTransformer
 
@@ -92,7 +93,7 @@ class ClassifierGenerator(BasicClassifier):
         return {'prediction_probability': predictions}
 
 
-class GloveEmbeddingsMatrix(BaseTransformer):
+class EmbeddingsMatrix(BaseTransformer):
     def __init__(self, pretrained_filepath, max_features, embedding_size):
         self.pretrained_filepath = pretrained_filepath
         self.max_features = max_features
@@ -105,6 +106,18 @@ class GloveEmbeddingsMatrix(BaseTransformer):
     def transform(self, tokenizer):
         return {'embeddings_matrix': self.embedding_matrix}
 
+    def _get_embedding_matrix(self, tokenizer):
+        return NotImplementedError
+
+    def save(self, filepath):
+        joblib.dump(self.embedding_matrix, filepath)
+
+    def load(self, filepath):
+        self.embedding_matrix = joblib.load(filepath)
+        return self
+
+
+class GloveEmbeddingsMatrix(EmbeddingsMatrix):
     def _get_embedding_matrix(self, tokenizer):
         embeddings_index = dict()
         with open(self.pretrained_filepath) as f:
@@ -122,14 +135,57 @@ class GloveEmbeddingsMatrix(BaseTransformer):
         nb_words = min(self.max_features, len(word_index))
         embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, self.embedding_size))
         for word, i in word_index.items():
-            if i >= self.max_features: continue
+            if i >= self.max_features:
+                continue
             embedding_vector = embeddings_index.get(word)
             if embedding_vector is not None: embedding_matrix[i] = embedding_vector
         return embedding_matrix
 
-    def save(self, filepath):
-        joblib.dump(self.embedding_matrix, filepath)
 
-    def load(self, filepath):
-        self.embedding_matrix = joblib.load(filepath)
-        return self
+class Word2VecEmbeddingsMatrix(EmbeddingsMatrix):
+    def _get_embedding_matrix(self, tokenizer):
+        model = KeyedVectors.load_word2vec_format(self.pretrained_filepath, binary=True)
+
+        emb_mean, emb_std = model.wv.syn0.mean(), model.wv.syn0.std()
+
+        word_index = tokenizer.word_index
+        nb_words = min(self.max_features, len(word_index))
+        embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, self.embedding_size))
+        for word, i in word_index.items():
+            if i >= self.max_features:
+                continue
+            try:
+                embedding_vector = model[word]
+                embedding_matrix[i] = embedding_vector
+            except KeyError:
+                continue
+        return embedding_matrix
+
+
+class FastTextEmbeddingsMatrix(EmbeddingsMatrix):
+    def _get_embedding_matrix(self, tokenizer):
+        embeddings_index = dict()
+        with open(self.pretrained_filepath) as f:
+            for i, line in enumerate(f):
+                line = line.strip()
+                if i == 0:
+                    continue
+                values = line.split(' ')
+                word = values[0]
+                coefs = np.asarray(values[1:], dtype='float32')
+                if coefs.shape[0] != self.embedding_size:
+                    continue
+                embeddings_index[word] = coefs
+
+        all_embs = np.stack(embeddings_index.values())
+        emb_mean, emb_std = all_embs.mean(), all_embs.std()
+
+        word_index = tokenizer.word_index
+        nb_words = min(self.max_features, len(word_index))
+        embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, self.embedding_size))
+        for word, i in word_index.items():
+            if i >= self.max_features:
+                continue
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+        return embedding_matrix
