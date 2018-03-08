ab836cb7ecea61ab8959bf7cbd091f9827c0a5c6
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2018-03-08 18:18:48 +0100

    fixed neptune channel naming for k-fold situations

--- steps/keras/callbacks.py
+++ steps/keras/callbacks.py
@@ -8,29 +8,21 @@ from keras.callbacks import Callback
 class NeptuneMonitor(Callback):
     def __init__(self, **kwargs):
         self.ctx = neptune.Context()
+        self.batch_loss_channel_name = get_correct_channel_name(self.ctx, 'Batch Log-loss training')
+        self.epoch_loss_channel_name = get_correct_channel_name(self.ctx, 'Log-loss training')
+        self.epoch_val_loss_channel_name = get_correct_channel_name(self.ctx, 'Log-loss validation')
+
         self.epoch_id = 0
         self.batch_id = 0
 
     def on_batch_end(self, batch, logs={}):
         self.batch_id += 1
-
-        channel_name_batch = 'Batch Log-loss training'
-        if self.batch_id == 0 and self.epoch_id == 0:
-            channel_name_batch = get_correct_channel_name(self.ctx, channel_name_batch)
-        self.ctx.channel_send(channel_name_batch, self.batch_id, logs['loss'])
+        self.ctx.channel_send(self.batch_loss_channel_name, self.batch_id, logs['loss'])
 
     def on_epoch_end(self, epoch, logs={}):
         self.epoch_id += 1
-
-        channel_name_epoch_train = 'Log-loss training'
-        if self.epoch_id == 0:
-            channel_name_epoch_train = get_correct_channel_name(self.ctx, channel_name_epoch_train)
-        self.ctx.channel_send(channel_name_epoch_train, self.epoch_id, logs['loss'])
-
-        channel_name_epoch_val = 'Log-loss validation'
-        if self.epoch_id == 0:
-            channel_name_epoch_val = get_correct_channel_name(self.ctx, channel_name_epoch_val)
-        self.ctx.channel_send(channel_name_epoch_val, self.epoch_id, logs['loss'])
+        self.ctx.channel_send(self.epoch_loss_channel_name, self.epoch_id, logs['loss'])
+        self.ctx.channel_send(self.epoch_val_loss_channel_name, self.epoch_id, logs['loss'])
 
 
 class ReduceLR(Callback):
