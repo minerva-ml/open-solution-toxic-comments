9f80f2eb644aa9fc7398ac58b6c09eb78db4d3f0
Shaz <shahebaz13@gmail.com>
2018-03-10 00:10:57 +0530

    Added APPO JSON file

--- steps/preprocessing.py
+++ steps/preprocessing.py
@@ -1,6 +1,6 @@
 import re
 import string
-
+import json
 import numpy as np
 import pandas as pd
 
@@ -18,7 +18,8 @@ tokenizer=TweetTokenizer()
 nltk.download('wordnet')
 nltk.download('stopwords')
 eng_stopwords = set(stopwords.words("english"))
-APPO = {"aren't": 'are not', "can't": 'cannot', "couldn't": 'could not', "didn't": 'did not', "doesn't": 'does not', "don't": 'do not', "hadn't": 'had not', "hasn't": 'has not', "haven't": 'have not', "he'd": 'he would', "he'll": 'he will', "he's": 'he is', "i'd": 'I had', "i'll": 'I will', "i'm": 'I am', "isn't": 'is not', "it's": 'it is', "it'll": 'it will', "i've": 'I have', "let's": 'let us', "mightn't": 'might not', "mustn't": 'must not', "shan't": 'shall not', "she'd": 'she would', "she'll": 'she will', "she's": 'she is', "shouldn't": 'should not', "that's": 'that is', "there's": 'there is', "they'd": 'they would', "they'll": 'they will', "they're": 'they are', "they've": 'they have', "we'd": 'we would', "we're": 'we are', "weren't": 'were not', "we've": 'we have', "what'll": 'what will', "what're": 'what are', "what's": 'what is', "what've": 'what have', "where's": 'where is', "who'd": 'who would', "who'll": 'who will', "who're": 'who are', "who's": 'who is', "who've": 'who have', "won't": 'will not', "wouldn't": 'would not', "you'd": 'you would', "you'll": 'you will', "you're": 'you are', "you've": 'you have', "'re": ' are', "wasn't": 'was not', "we'll": ' will'}
+APPO = json.load(open('../external_data/apostrophes.json'))
+
 
 class WordListFilter(BaseTransformer):
     def __init__(self, word_list_filepath):
